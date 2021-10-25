8b73f99feeb3adc5489e425860f0fc471d524df1
CEK19 <nguyentrongnhan2001.work@gmail.com>
2021-10-25 20:39:31 +0700

    [NAVBAR + SALE PAGE]-[navbar.html, sale.html, footer.html]-[Update navbar+footer and Sale Page

--- js/footer.js
+++ js/footer.js
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/footer.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>        
-    <title>Document</title>
-</head>
-<body>
-    <footer>
+function getFooter(){
+    let obj = document.getElementsByClassName("myFooter")[0];
+    if(obj == null) alert("FOOTER ERROR");
+    else{
+        obj.innerHTML = `
+        <footer>
         <hr>
         <div class="payment-social">
             <div class="social">
@@ -106,6 +99,9 @@
                 </div>
             </div>
         </div>
-    </footer>
-</body>
-</html>
+    </footer>   
+        `;
+    }
+}
+
+getFooter();
