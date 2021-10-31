b007eb42c426d003dbe983825e42d3e0c7552745
MinhTuLeHoang <tu.le_handspro@hcmut.edu.vn>
2021-10-31 11:42:10 +0700

    Del

--- mvc_demo/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-require_once "./mvc/Bridge.php";
-$myApp = new App();
-?>
--- mvc_demo/mvc/Bridge.php
+++
@@ -1,4 +0,0 @@
-<?php
-    require_once "./mvc/core/App.php";
-    require_once "./mvc/core/Controller.php";
-?>
