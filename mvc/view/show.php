a72ab4be9c666f26cb7b7ea7557035bdea45a83e
MinhTuLeHoang <tu.le_handspro@hcmut.edu.vn>
2021-11-19 12:02:05 +0700

    [css WishList] [wishList] [test]

--- mvc/view/show.php
+++ mvc/view/show.php
@@ -8,6 +8,7 @@
     <!-- -----------------Library--------------------- -->
     <link type="text/css" rel="stylesheet" href="./public/css/navbar.css">
     <link type="text/css" rel="stylesheet" href="./public/css/footer.css">
+    <link type="text/css" rel="stylesheet" href="./public/css/wishList.css">
     <link type="text/css" rel="stylesheet" href="./public/css/sale.css">
     <link type="text/css" rel="stylesheet" href="./public/css/home_page.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
