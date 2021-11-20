2f72ee91d2ca6a4b54b5ec4b5bbcbc74510d3747
trongho912 <hohuutrong912@gmail.com>
2021-11-20 13:43:04 +0700

    test repush

---
+++ mvc/views/page/product-detail.php
@@ -0,0 +1,146 @@
+<div id="sale-page-background">
+<!-- -----------------INFORMATION--------------------- -->
+    <div class="information">   
+        <div class="in4 r-border">
+            <div class="block-text">
+                <p>EXTRA 20% OFF</p>
+                <p>CLICK HERE FOR MORE INFORMATION</p>
+            </div>
+        </div>
+        <div class="in4 l-border">
+            <div class="block-text">
+                <p>UP TO 50% OF SWEET STYLES</p>
+            </div>
+        </div>
+    </div>    
+    
+<!-- ----------------INTRODUCE PRODUCT---------------- -->
+<div class="product-detail-container">
+    <!-- 1 sản phẩm -->
+    <div class="product-detail-frame mb-3">
+        <div class="product-detail-img-here">
+                    <!--IMAGE HERE-->
+        </div>
+        <div style="width:50%; padding: 0 25px 0 75px;">
+            <h4>Tên của sản phẩm người ta</h4>
+            <div class="d-flex flex-row mt-2">
+                <p class="text-decoration-line-through fst-italic fs-5 mb-0 me-5 align-middle">178.000Đ<p>
+                <p class="fw-bolder fs-4 mb-0 align-middle">150.000Đ</p>
+            </div>
+            <hr class="mt-2 mb-2">
+            <div class="">
+
+            </div>
+            <table class="table table-borderless">
+                <tbody>
+                    <tr class="border-bottom">
+                        <th class="align-middle fs-5">Size</th>
+                        <td>    
+                            <button type="button" class="btn-sm btn-light btn-outline-secondary shadow" style="width: 45px; height:45px;">S</button>
+                        </td>
+                        <td>
+                            <button type="button" class="btn-sm btn-light btn-outline-secondary shadow" style="width: 45px; height:45px;">M</button>
+                        </td>
+                        <td>
+                            <button type="button" class="btn-sm btn-light btn-outline-secondary shadow" style="width: 45px; height:45px;">L</button>
+                        </td>
+                        <td>
+                            <button type="button" class="btn-sm btn-light btn-outline-secondary shadow" style="width: 45px; height:45px;">XL</button>
+                        </td>
+                    </tr>
+                    <tr class="border-top">
+                        <th class="align-middle fs-5">Color</th>
+                        <td>
+                            <button type="button" class="btn-lg btn-dark shadow border border-4 border-primary" style="width: 45px; height:45px;"></button>
+                        </td>
+                        <td>
+                            <button type="button" class="btn-lg btn-light shadow border border-dark" style="width: 45px; height:45px;"></button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <hr class="mt-2 mb-2">
+            <br>
+            <button type="button" class="btn-sm btn-light btn-outline-dark border-4 mt-2" style="width: 100%; border-radius: 15px;">
+                <div class="mb-0"  style="font-size: 25px; font-weight:bold;">
+                    MOVE TO BAG
+                </div>
+            </button>
+            <br><br>
+            <button type="button" class="btn-sm btn-light btn-outline-dark border-4 mt-2" style="width: 100%; border-radius: 15px; ">
+                <div class="mb-0" style="font-size: 25px; font-weight:bold;">
+                    FAVORITE <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" width="35" height="35" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512"><g transform="translate(512 0) scale(-1 1)"><path fill="currentColor" d="M453.122 79.012a128 128 0 0 0-181.087.068l-15.511 15.7l-15.382-15.666l-.1-.1a128 128 0 0 0-181.02 0l-6.91 6.91a128 128 0 0 0 0 181.019l182.373 182.371l20.595 21.578l.491-.492l.533.533l19.296-20.359L460.032 266.94a128.147 128.147 0 0 0 0-181.019zM437.4 244.313L256.571 425.146L75.738 244.313a96 96 0 0 1 0-135.764l6.911-6.91a96 96 0 0 1 135.713-.051l38.093 38.787l38.274-38.736a96 96 0 0 1 135.765 0l6.91 6.909a96.11 96.11 0 0 1-.004 135.765z"/></g></svg>
+                </div>
+            </button>
+        </div> 
+    </div>
+    <hr class="mt-2 mb-2">
+    <div class="product-detail-more-info mb-3">
+        <div class="row">
+            <div class="col">
+                <p style=" color: #999999; font-weight: bold;">PRODUCT DETAILS </p>
+                some silly stuff some silly stuff some silly stuff some silly stuff 
+                some silly stuff <br>
+                some silly stuff <br>
+                some silly stuff <br>
+                some silly stuff <br>
+                some silly stuff <br>
+                some silly stuff <br>
+            </div>
+            <div class="col">
+                <div class="row" style="padding-bottom: 70px">
+                   <p style="color: #999999; font-weight: bold; ">PRODUCT CODE </p>
+                   <span style="color: blue; font-weight: bold">#1915672</span>
+                </div>
+                <div class="row">
+                    <p style="color: #999999; font-weight: bold;">BRAND </p>
+                    <span>adidas</span>
+                </div>
+            </div>
+            <div class="col">
+                <p style=" color: #999999; font-weight: bold;">ABOUT ME </p>
+            </div>
+        </div>
+    </div>
+    <hr class="mt-2 mb-2">
+    <div class="product-detail-comments">
+        <p style="color: #999999; font-weight: bold; ">REVIEWS </p>
+
+        <div class="product-detail-comments-frame">
+            <div class="product-detail-comments-account">
+                <div class="product-detail-comments-avatar ">
+                <!---- avatar hear ----> 
+                </div>
+                
+                <div style="display: flex; flex-direction: column;">
+                    <div class="product-detail-comments-name">
+
+                    </div>
+                    <div class="product-detail-date-cmt">
+
+                    </div>
+                </div>
+            </div>
+            <div class="product-detail-comment">
+
+            </div>
+        </div>
+        <br>
+        <div class="product-detail-comments-frame">
+            hafklsahfla
+        </div>
+            <div style="margin: auto">
+                <button type="button" class="btn-sm btn-light btn-outline-dark border-4 mt-2" style="width: 300px;">
+                    <div class="mb-0"  style="font-size: 20px; font-weight:bold;">
+                        LOAD MORE
+                    </div> 
+                </button>
+            </div>    
+    </div>
+</div>
+
+
+
+    
+</div>
+        
