b83ad95916cc01ab1d7f4f524cad39d3ec967594
MinhTuLeHoang <tu.le_handspro@hcmut.edu.vn>
2021-11-21 16:09:56 +0700

    [frontend] [shopping_bag] [not done yet]

--- mvc/views/page/shopping_bag.php
+++ mvc/views/page/shopping_bag.php
@@ -1,4 +1,4 @@
-<div id="sale-page-background">
+<div id="sale-page-background" class="Sb-page-background pb-5">
     <!-- -----------------INFORMATION--------------------- -->
     <div class="information">
         <div class="in4 r-border">
@@ -16,6 +16,54 @@
 
     <!-- -----------------SAVE ITEMS-------------------- -->
 
-    
+    <!-- Sb: Shopping_bag -->
+    <div class="container Sb-container mt-3 d-flex flex-row justify-content-between">
+        <div class="Sb-left">
+            <div class="ps-3 pt-2 pb-2">
+                <h3 class="mb-0">MY BAG</h3>
+            </div>
+
+            <div class="mt-3 ps-2 pt-2 pb-2">
+
+                <div class="Sb-frame d-flex flex-row">
+                    <div class="border Sb-img">
+                        <!-- img -->
+                        hình
+                    </div>
+                    <div class="Sb-detail ms-2">
+                        <h4>45.00 $</h4>
+                        <p>Đây là tên của sản phẩm người ta</p>
+
+                        <div class="d-flex row ms-0 me-0">
+                            <select class="form-select me-2" style="width:20%">
+                                <option value="1" selected>Black</option>
+                                <option value="2">White</option>
+                            </select>
+
+                            <select class="form-select" style="width:20%">
+                                <option value="1">S</option>
+                                <option value="2" selected>M</option>
+                                <option value="3">L</option>
+                                <option value="4">XL</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="Sb-frame">
+                    Teen, caos lfsdjalf
+                </div>
+            </div>
+        </div>
+
+
+        <div class="Sb-right">
+            <div class="ps-3 pt-2 pb-2">
+                <h4 class="mb-0">TOTAL</h4>
+            </div>
+        </div>
+    </div>
 
 </div>
