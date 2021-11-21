8f6d6af750d29003798b337ed275546cd0201dea
vucong2018 <vucong2018@gmail.com>
2021-11-21 16:21:33 +0700

    register feature
    [Register]

--- mvc/views/show.php
+++ mvc/views/show.php
@@ -1,16 +1,23 @@
+<?php
+require_once "./mvc/core/basehref.php";
+$home_url = getUrl().'/';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<?php
+        echo "<base href='${home_url}'>";
+    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- -----------------Library--------------------- -->
-    <link type="text/css" rel="stylesheet" href="./public/css/navbar.css">
-    <link type="text/css" rel="stylesheet" href="./public/css/footer.css">
-    <link type="text/css" rel="stylesheet" href="./public/css/wishList.css">
-    <link type="text/css" rel="stylesheet" href="./public/css/sale.css">
-    <link type="text/css" rel="stylesheet" href="./public/css/home_page.css">
+    <link type="text/css" rel="stylesheet" href="public/css/navbar.css">
+    <link type="text/css" rel="stylesheet" href="public/css/footer.css">
+    <link type="text/css" rel="stylesheet" href="public/css/wishList.css">
+    <link type="text/css" rel="stylesheet" href="public/css/sale.css">
+    <link type="text/css" rel="stylesheet" href="public/css/home_page.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>        
 
