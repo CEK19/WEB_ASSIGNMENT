cb1d27b9d6fbaa5559d7b6ab7e439ec2fa35b271
vucong2018 <vucong2018@gmail.com>
2021-11-21 16:50:55 +0700

    [link css 2 in 1]
    [link css] [show.php]

--- mvc/views/show.php
+++ mvc/views/show.php
@@ -13,20 +13,20 @@ $home_url = getUrl().'/';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- -----------------Library--------------------- -->
-<<<<<<< HEAD
+
     <link type="text/css" rel="stylesheet" href="public/css/navbar.css">
     <link type="text/css" rel="stylesheet" href="public/css/footer.css">
     <link type="text/css" rel="stylesheet" href="public/css/wishList.css">
     <link type="text/css" rel="stylesheet" href="public/css/sale.css">
     <link type="text/css" rel="stylesheet" href="public/css/home_page.css">
-=======
+
     <link type="text/css" rel="stylesheet" href="./public/css/navbar.css">
     <link type="text/css" rel="stylesheet" href="./public/css/footer.css">
     <link type="text/css" rel="stylesheet" href="./public/css/wishList.css">
     <link type="text/css" rel="stylesheet" href="./public/css/shopping_bag.css">
     <link type="text/css" rel="stylesheet" href="./public/css/sale.css">
     <link type="text/css" rel="stylesheet" href="./public/css/home_page.css">
->>>>>>> b83ad95916cc01ab1d7f4f524cad39d3ec967594
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>        
 
