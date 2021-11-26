8b1a352aa8f917e3045609b06c1ae8361ab7082a
CEK19 <nguyentrongnhan2001.work@gmail.com>
2021-11-26 21:20:44 +0700

    [MOVE TO BAG BUTTON]

--- mvc/core/AJAX/moveToBag.php
+++ mvc/core/AJAX/moveToBag.php
@@ -3,30 +3,41 @@
     session_start();
 
     class BagAdd extends db{
+
+        public function is_exist_item_shoppingbag_base_email($clothID, $category, $email){                                                
+            $format_EMAIL = "'".$email."'";
+            $format_CATEGORY = "'".$category."'";
+            $SQL_EXIST = "SELECT email, clothID, category FROM shopping_bag WHERE email={$format_EMAIL} AND clothID={$clothID} AND category={$format_CATEGORY}"; 
+            $result = mysqli_query($this->connect, $SQL_EXIST);                        
+            if (mysqli_num_rows($result) > 0) {
+                return true;
+            }            
+            return false;            
+        }
+
         public function addShoppingBag($clothID, $category, $email){
             $format_CATEGORY = "'".$category."'";
             $format_EMAIL = "'".$email."'";            
             $responseData  = array(                
                 "is_exist" => "exist"
-            );
-            // CASE 1: IF EXIST IN BAG => RETURN WARNING CODE
-            $SQL_QUERY_SEARCH = "SELECT email, clothID, category FROM shopping_bag";
-            $result_search = mysqli_query($this->connect, $SQL_QUERY_SEARCH);
-            if(mysqli_num_rows($result_search) > 0){
-                return json_encode($responseData);
-            }
-
-            // CASE 2: IF NOT EXIST IN BAG => ADD TO BAG DATABASE
-            $SQL_QUERY_ADD_BAG = "INSERT INTO shopping_bag (email, clothID, category) VALUES ({$format_EMAIL}, {$clothID}, {$format_CATEGORY})";
+            );                                    
+            $SQL_QUERY_ADD_BAG = "INSERT INTO shopping_bag (email, clothID, category, color, size, quantity) VALUES ({$format_EMAIL}, {$clothID}, {$format_CATEGORY}, 'black', 'M', 1)";
             if(mysqli_query($this->connect, $SQL_QUERY_ADD_BAG)){
                 $responseData['is_exist'] = "non-exist";
             }
-            return $responseData;
+            return json_encode($responseData);
         }
     }
 
     // $addObj = new WishListAdd();
     // echo $addObj->addWL($_POST['ajax_clothid'], $_POST['ajax_category'], $_SESSION['email']);
     $addBagObj = new BagAdd();
-    echo $addBagObj->addShoppingBag($_POST['ajax_clothid'], $_POST['ajax_category'], $_SESSION['email']);
+    if($addBagObj->is_exist_item_shoppingbag_base_email($_POST['ajax_clothid'], $_POST['ajax_category'], $_SESSION['email'])){
+        echo json_encode(array(
+            "is_exist" => "exist"
+        ));
+    }   
+    else{
+        echo $addBagObj->addShoppingBag($_POST['ajax_clothid'], $_POST['ajax_category'], $_SESSION['email']);
+    }
 ?>
--- public/js/wishList.js
+++ public/js/wishList.js
@@ -25,7 +25,58 @@ $(document).ready(function() {
                 // STEP 3: UPDATE COLOR OF BUTTON         
                 if(result.state_response == "successful"){                                        
                 }
-            });                        
+            });             
+
             $(this).closest(".wishList-frame").remove();
+        })
+
+    $("#wishList-page-background button")
+        .click(function(e){
+            e.preventDefault();
+
+            let buttonObj  = $(this);
+
+            let frameObj = $(this).closest(".wishList-frame");
+            let pathImg = frameObj.children().children("img").attr("src");
+            let splitPathArr = pathImg.split('/');
+            let category = splitPathArr[3];
+            let clothID = splitPathArr[4].replace(".jpeg", ""); 
+            let isWarning = false;
+
+            jQuery.ajaxSetup({async:false});
+
+            $.ajax({
+                url: './mvc/core/AJAX/moveToBag.php',
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    ajax_category: category,
+                    ajax_clothid: clothID
+                }
+            }).done(function(result) {
+                // STEP 3: UPDATE COLOR OF BUTTON         
+                if(result.is_exist == "exist"){
+                    isWarning = true;                                        
+                    alert("Your item have been existed in your shopping bag !");
+                }
+            });   
+            
+
+            if(!isWarning){
+                $.ajax({
+                    url: './mvc/core/AJAX/delWishlist.php',
+                    type: 'POST',
+                    dataType: 'json',
+                    data: {
+                        ajax_category: category,
+                        ajax_clothid: clothID
+                    }
+                }).done(function(result_delete) {                        
+                    if(result_delete.state_response == "successful"){                                        
+                    }
+                });  
+                $(this).closest(".wishList-frame").remove();                
+            }
+            jQuery.ajaxSetup({async:true});
         })
 });
