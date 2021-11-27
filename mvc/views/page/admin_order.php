6c4ef7f97fd769c95b58e12913d11995cb1b6ae8
MinhTuLeHoang <tu.le_handspro@hcmut.edu.vn>
2021-11-27 10:58:19 +0700

    [edit front end] [admin order]

--- mvc/views/page/admin_order.php
+++ mvc/views/page/admin_order.php
@@ -8,131 +8,8 @@
     <div class="admin-container bg-white pt-4 ps-5 pe-5">
         <form>
             <div class="aa-top d-flex ps-5">
-                <div class="aa-left">
-                    <h3>Add new order</h3>
-
-                    <div>
-                        <label class="form-label col-auto">email</label>
-                        <div class="col-8 mb-2">
-                            <input class="form-control" type="text">
-                        </div>
-                    </div>
-
-                    <div class="d-flex mb-2">
-                        <div style="width: 20%;">
-                            <label class="form-label col-auto">clothID</label>
-                            <div class="col-10">
-                                <input class="form-control" type="text">
-                            </div>
-                        </div>
-                        <div style="width: 70%;">
-                            <label class="form-label col-auto">category</label>
-                            <div class="col-8">
-                                <select class="form-select">
-                                    <option selected>Choose one category</option>
-                                    <option value="access_bag">access_bag</option>
-                                    <option value="access_caps">access_caps</option>
-                                    <option value="access_glass">access_glass</option>
-                                    <option value="pants_jeans">pants_jeans</option>
-                                    <option value="pants_jog">pants_jog</option>
-                                    <option value="pants_lounges">pants_lounges</option>
-                                    <option value="shirt_polo">shirt_polo</option>
-                                    <option value="shirt_shirt">shirt_shirt</option>
-                                    <option value="shirt_tshirt_vest">shirt_tshirt_vest</option>
-                                    <option value="shoes_sandals_slippers">shoes_sandals_slippers</option>
-                                    <option value="shoes_shocks">shoes_shocks</option>
-                                    <option value="shoes_trainers">shoes_trainers</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div class="d-flex mb-2">
-                        <div style="width: 24%;">
-                            <label class="form-label col-auto">color</label>
-                            <div class="col-10">
-                                <select class="form-select">
-                                    <option value="black" selected>black</option>
-                                    <option value="white">white</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div style="width: 24%;">
-                            <label class="form-label col-auto">size</label>
-                            <div class="col-10">
-                                <select class="form-select">
-                                    <option value="black">S</option>
-                                    <option value="white" selected>M</option>
-                                    <option value="white">L</option>
-                                    <option value="white">XL</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div style="width: 23%;">
-                            <label class="form-label col-auto">quantity</label>
-                            <div class="col-10">
-                                <input class="form-control" type="text">
-                            </div>
-                        </div>
-                    </div>
-
-                    <label class="form-label col-auto">phone</label>
-                    <div class="col-8 mb-2">
-                        <input class="form-control" type="text">
-                    </div>
-
-                    <label class="form-label col-auto">address</label>
-                    <div class="col-8 mb-2">
-                        <input class="form-control" type="text">
-                    </div>
-
-
-                    <div class="d-flex mb-2">
-                        <div style="width: 30%;">
-                            <label class="form-label col-auto">deliveryMethod</label>
-                            <div class="col-10 mb-2">
-                                <select class="form-select">
-                                    <option value="fast" selected>fast</option>
-                                    <option value="normal">normal</option>
-                                </select>
-                            </div>
-                        </div>
-
-                        <div style="width: 40%;">
-                            <label class="form-label col-auto">paymentMethod</label>
-                            <div class="col-8 mb-2">
-                                <select class="form-select">
-                                    <option value="black">card</option>
-                                    <option value="white">e-wallet</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-
-
-
-                    <label class="form-label col-auto">Card Number</label>
-                    <div class="col-8 mb-2">
-                        <input class="form-control" type="text">
-                    </div>
-
-                    <label class="form-label col-auto">e-wallet</label>
-                    <div class="col-8 mb-2">
-                        <input class="form-control" type="text">
-                    </div>
-
-                    <div class="d-grid col-8">
-                        <button class="btn btn-success">ADD</button>
-                    </div>
-                </div>
-
-
-
-                <div class="aa-right">
-                    <h3>Edit existed order</h3>
+                <div style="width: 80%;">
+                    <h3 class="mb-3">Edit existed order</h3>
 
                     <div class="d-flex">
                         <div style="width: 20%;">
@@ -145,7 +22,7 @@
                         <div style="width: 70%;">
                             <label class="form-label col-auto">email</label>
                             <div class="col-8 mb-2">
-                                <input class="form-control" type="text">
+                                <input class="form-control" type="email">
                             </div>
                         </div>
                     </div>
@@ -155,7 +32,7 @@
                         <div style="width: 20%;">
                             <label class="form-label col-auto">clothID</label>
                             <div class="col-10">
-                                <input class="form-control" type="text">
+                                <input class="form-control" type="number">
                             </div>
                         </div>
                         <div style="width: 70%;">
@@ -207,14 +84,14 @@
                         <div style="width: 23%;">
                             <label class="form-label col-auto">quantity</label>
                             <div class="col-10">
-                                <input class="form-control" type="text">
+                                <input class="form-control" type="number">
                             </div>
                         </div>
                     </div>
 
                     <label class="form-label col-auto">phone</label>
                     <div class="col-8 mb-2">
-                        <input class="form-control" type="text">
+                        <input class="form-control" type="number">
                     </div>
 
                     <label class="form-label col-auto">address</label>
