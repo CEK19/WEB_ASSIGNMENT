10285ceb6b13631d2dc861c7e9cc8669b23f99e7
MinhTuLeHoang <tu.le_handspro@hcmut.edu.vn>
2021-11-29 11:03:00 +0700

    [fix bug] [sale.php]

--- mvc/views/page/sale.php
+++ mvc/views/page/sale.php
@@ -45,74 +45,86 @@
         <div class="introduce-hot">
             <div class="frame mb-3">
                 <div class="img-here">
+                <a href="./?url=Home/productDetail/shirt_polo/10">
                     <img src="./public/image/sale_page_img/1.jpeg" style="width:100%;">
+                </a>
                 </div>
                 <div class="align-middle d-flex align-items-center" style="width: 100%; height: 87px;">
                     <h1>FASHIONAL & STYLISTIC</h1>
                 </div>
                 <!-- <button type="button" class="btn btn-outline-dark"> -->
-                <button type="button" class="btn btn-light btn-outline-dark border-4 mt-2" style="width: 70%;">
-                    <a href="./?url=Home/shirt_polo"><h2>SHOP NOW</h2></a>
+                <button type="button" class="btn btn-light btn-outline-dark border-4 mt-2" onclick="location.href='./?url=Home/shirt_polo'" style="width: 70%;">
+                    <h2>SHOP NOW</h2>
                 </button>
             </div>
 
             <div class="frame mb-3">
                 <div class="img-here">
-                <img src="./public/image/sale_page_img/2.jpeg" style="width:100%;">
+                <a href="./?url=Home/productDetail/shoes_sandals_slippers/17">
+                    <img src="./public/image/sale_page_img/2.jpeg" style="width:100%;">
+                </a>
                 </div>
                 <div class="align-middle d-flex align-items-center" style="width: 100%; height: 87px;">
                     <h1>GAMINE & SPORTY</h1>
                 </div>
-                <button type="button" class="btn btn-light btn-outline-dark border-4 mt-2" style="width: 70%;">
-                    <a href="./?url=Home/shoes_sandals_slippers"><h2>SHOP NOW</h2></a>
+                <button type="button" class="btn btn-light btn-outline-dark border-4 mt-2" onclick="location.href='./?url=Home/shoes_sandals_slippers'" style="width: 70%;">
+                    <h2>SHOP NOW</h2>
                 </button>
             </div>
 
             <div class="frame mb-3">
                 <div class="img-here">
-                <img src="./public/image/sale_page_img/3.jpeg" style="width:100%;">
+                <a href="./?url=Home/productDetail/access_glass/10">
+                    <img src="./public/image/sale_page_img/3.jpeg" style="width:100%;">
+                </a>
                 </div>
                 <div class="align-middle d-flex align-items-center" style="width: 100%; height: 87px;">
                     <h1>CLASSIC & GLAMOROUS</h1>
                 </div>
-                <button type="button" class="btn btn-light btn-outline-dark border-4 mt-2" style="width: 70%;">
-                    <a href="./?url=Home/access_glass"><h2>SHOP NOW</h2></a>
+                <button type="button" class="btn btn-light btn-outline-dark border-4 mt-2" onclick="location.href='./?url=Home/access_glass'" style="width: 70%;">
+                    <h2>SHOP NOW</h2>
                 </button>
             </div>
 
             <div class="frame mb-3">
                 <div class="img-here">
-                <img src="./public/image/sale_page_img/4.jpeg" style="width:100%;">
+                <a href="./?url=Home/productDetail/shirt_shirt/14">
+                    <img src="./public/image/sale_page_img/4.jpeg" style="width:100%;">
+                </a>
                 </div>
                 <div class="align-middle d-flex align-items-center" style="width: 100%; height: 87px;">
                     <h1>DRAMATIC & TRENDY</h1>
                 </div>
-                <button type="button" class="btn btn-light btn-outline-dark border-4 mt-2" style="width: 70%;">
-                    <a href="./?url=Home/shirt_shirt"><h2>SHOP NOW</h2></a>
+                <button type="button" class="btn btn-light btn-outline-dark border-4 mt-2" onclick="location.href='./?url=Home/shirt_shirt'" style="width: 70%;">
+                    <h2>SHOP NOW</h2>
                 </button>
             </div>
 
             <div class="frame mb-3">
                 <div class="img-here">
-                <img src="./public/image/sale_page_img/5.jpeg" style="width:100%;">
+                <a href="./?url=Home/productDetail/shirt_polo/7">
+                    <img src="./public/image/sale_page_img/5.jpeg" style="width:100%;">
+                </a>
                 </div>
                 <div class="align-middle d-flex align-items-center" style="width: 100%; height: 87px;">
                     <h1>NATURAL & COMMODIOUS</h1>
                 </div>
-                <button type="button" class="btn btn-light btn-outline-dark border-4 mt-2" style="width: 70%;">
-                    <a href="./?url=Home/shirt_polo"><h2>SHOP NOW</h2></a>
+                <button type="button" class="btn btn-light btn-outline-dark border-4 mt-2" onclick="location.href='./?url=Home/shirt_polo'" style="width: 70%;">
+                    <h2>SHOP NOW</h2>
                 </button>
             </div>
 
             <div class="frame mb-3">
                 <div class="img-here">
-                <img src="./public/image/sale_page_img/6.jpeg" style="width:100%;">
+                <a href="./?url=Home/productDetail/shirt_shirt/13">
+                    <img src="./public/image/sale_page_img/6.jpeg" style="width:100%;">
+                </a>
                 </div>
                 <div class="align-middle d-flex align-items-center" style="width: 100%; height: 87px;">
                     <h1>WARM & ARTY</h1>
                 </div>
-                <button type="button" class="btn btn-light btn-outline-dark border-4 mt-2" style="width: 70%;">
-                    <a href="./?url=Home/shirt_shirt"><h2>SHOP NOW</h2></a>
+                <button type="button" class="btn btn-light btn-outline-dark border-4 mt-2" onclick="location.href='./?url=Home/shirt_shirt'" style="width: 70%;">
+                    <h2>SHOP NOW</h2>
                 </button>
             </div>
         </div>
